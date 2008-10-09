122cd66a0a680c11e470bdc55d2e3b0487b7e235
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-10-08 22:46:45 -0400

    Made *examples* be a lexically closed variable instead

--- clspec.lisp
+++ clspec.lisp
@@ -4,28 +4,31 @@
   (:export describe run-examples))
 (in-package #:clspec)
 
-(defmacro add-example (example)
-  `(progn (defvar *examples* ())
-	  (setf *examples*
-		(nconc *examples* (list (lambda () ,example))))))
+(let ((examples (list)))
+  (defmacro add-example (example)
+    (setf examples
+	  (nconc examples (list (lambda () example))))
+    nil)
 
-(defmacro describe (description behavior)
-  (list 'add-example (cons '=> (rest (second behavior)))))
+  (defmacro describe (description behavior)
+    (list 'add-example (cons '=> (rest (second behavior)))))
 
-(defmacro => (form should matcher result)
-  `(equalp ,form ,result))
+  (defmacro => (form should matcher result)
+    `(equalp ,form ,result))
+
+  (defun run-examples ()
+    (loop for example in examples
+       for result = (funcall example)
+       count result into pass-count
+       do (princ (if result "." "F"))
+       finally (princ (format nil
+			      "~%~%~D Examples, ~D Failures"
+			      pass-count
+			      (- (length examples) pass-count))))  
+    (values)))
 
 (clspec:describe "append"
   ("should add two lists"
     (=> (append '(1 2) '(3 4)) should = '(1 2 3 4))))
 
-(defun run-examples ()
-  (loop for example in *examples*
-        for result = (funcall example)
-        count result into pass-count
-        do (princ (if result "." "F"))
-        finally (princ (format nil
-			       "~%~%~D Examples, ~D Failures"
-			       pass-count
-			       (- (length *examples*) pass-count))))  
-  (values))
+(clspec:run-examples)
