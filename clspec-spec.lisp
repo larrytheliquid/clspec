340b508873c3035b51d8f4b29d584d38a57459f3
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-10-11 17:00:48 -0400

    Got top-level describe working

--- clspec-spec.lisp
+++ clspec-spec.lisp
@@ -1,15 +1,21 @@
-(clspec:clear-examples)
+(defpackage #:spec
+  (:use #:common-lisp)
+  (:shadowing-import-from #:clspec #:describe)
+  (:use #:clspec))
+(in-package :spec)
 
-(clspec:describe "#'describe, without examples")
+(clear-examples)
 
-(clspec:describe "#'describe, with 1 example"
-  (clspec:it "should run the single example"
-     (clspec:=> (append '(1 2) '(3 4)) should = '(1 2 3 4))))
+(describe "describe, without examples")
 
-(clspec:describe "#'describe, with 2 examples"
-  (clspec:it "should run the first example"
-     (clspec:=> (1+ 6) should = 7))
-  (clspec:it "should run the second example"
-     (clspec:=> (1- 6) should = 5)))
+(describe "describe, with 1 example"
+  (it "should run the single example"
+    (=> (append '(1 2) '(3 4)) should = '(1 2 3 4))))
 
-(clspec:run-examples)
+(describe "describe, with 2 examples"
+  (it "should run the first example"
+    (=> (1+ 6) should = 7))
+  (it "should run the second example"
+    (=> (1- 6) should = 5)))
+
+(run-examples)
--- package.lisp
+++ package.lisp
@@ -1,6 +1,6 @@
 (defpackage #:clspec
   (:use #:common-lisp)
-  (:shadow describe)
-  (:export describe it =>
-	   run-examples clear-examples
-	   enqueue))
+  (:shadow #:describe)
+  (:export #:describe #:it #:=>
+	   #:run-examples #:clear-examples
+	   #:enqueue))
