339d512d07edd26df530bf415d9dec478c877a70
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-10-13 16:41:32 -0400

    Added example-group class and description methods

--- lib/example-group.lisp
+++ lib/example-group.lisp
@@ -1,4 +1,9 @@
 (in-package :clspec)
 
-(defun example-group ()
-  "example group")
+(defclass example-group ()
+  ((description :initarg :description
+		:accessor description)))
+
+(defmethod elaborate-description ((e-g example-group) elaboration)
+  (setf (description e-g)
+	(concatenate 'string (description e-g) " " elaboration)))
--- spec/example-group-spec.lisp
+++ spec/example-group-spec.lisp
@@ -1,5 +1,20 @@
 (clspec:spec)
 
 (describe "example-group"
-  (it "should be testable without being exported"
-    (=> (example-group) should = "example group")))
+  (describe "description"
+    (it "should be readable"
+      (let ((example-group (make-instance 'example-group
+					  :description "description")))
+	(=> (description example-group) should = "description")))
+
+    (it "should be writeable"
+      (let ((example-group (make-instance 'example-group
+					  :description "description")))
+	(setf (description example-group) "changed description")
+	(=> (description example-group) should = "changed description")))
+
+    (it "should be elaborateable"
+      (let ((example-group (make-instance 'example-group
+					  :description "description")))
+	(elaborate-description example-group "elaborated")
+	(=> (description example-group) should = "description elaborated")))))
