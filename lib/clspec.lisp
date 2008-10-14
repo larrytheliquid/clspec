bd1499c669a4051c9f511dca24962e686f2523d1
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-10-13 23:58:11 -0400

    Display a summary of failures after specs run

--- lib/clspec.lisp
+++ lib/clspec.lisp
@@ -29,20 +29,32 @@
 
   (defun run-examples ()
     (unless (null example-groups)
-      (let ((examples-count 0) (failures-count 0))
+      (let ((examples-count 0) (failures-count 0) (failures-summary ""))
 	(dolist (example-group example-groups)
 	  (format t "~%~A~%" (description example-group))
 	  (loop for example in (examples example-group)
 	     for success = (eval (behavior example))
 	     do (incf examples-count)
 	     unless success do (incf failures-count)
+	       and do (setf failures-summary
+			 (concatenate 'string failures-summary
+				      (summarize-failure example-group
+							 example
+							 failures-count)))
 	     do (format t "- ~A~A~%" (description example)
 			           (if success ""
 				       (format nil " (FAILED - ~D)"
 					           failures-count)))))
+	(unless (equal failures-summary "")
+	  (format t "~%~A~%" failures-summary))
 	(format t "~%~D Examples, ~D Failures~%"
-		  examples-count failures-count)))
+		examples-count failures-count)))
     (values))
+
+  (defun summarize-failure (example-group example failure-num)
+    (format nil "~D)~%'~A ~A' FAILED" failure-num
+	    (description example-group)
+	    (description example)))
 
   (defun clear-examples ()
     (setf shared-examples (make-hash-table :test #'equal))
